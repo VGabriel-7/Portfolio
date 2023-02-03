fac3a95cd6d7542d97abaf87d3269401a7829425
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-02-03 08:07:04 -0300

    Iniciando meu portfolio :rocket:.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './assets/index.css'
+
+function App() {
+  return (
+    <h1
+      className="font-extrabold text-center text-violet-700 text-3xl"
+    >
+      Hello world!
+      With tailwind
+    </h1>
+  )
+}
+
+export default App
